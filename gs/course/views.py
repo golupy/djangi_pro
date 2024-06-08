d2b15aa5b15ae213216e5713496576418856a262
golu <golupyproject@gmail.com>
2024-06-08 16:43:01 +0530

    create view function for course app

--- gs/course/views.py
+++ gs/course/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request,'course/index.html')
 # Create your views here.
+
+def course_details(request):
+    return render(request,'course/course_detail.html')
