c0ba595d14c78becbcf84bdfa2bc12ae12449d52
golu <golupyproject@gmail.com>
2024-06-08 19:32:04 +0530

    create a model Students

--- gs/student/admin.py
+++ gs/student/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Student
 # Register your models here.
+admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('roll','name')
