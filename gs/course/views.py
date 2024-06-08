596b16d0036a99ba5d658aaa65a80de6f558200f
golupy <golupyproject@gmail.com>
2024-06-08 17:09:36 +0530

    create a course_dur fun inside views of course app

--- gs/course/views.py
+++ gs/course/views.py
@@ -8,3 +8,7 @@ def course_details(request):
     course=['Python','Django','PostgreSql','Mysql','Git&Github']
     #course=""
     return render(request,'course/course_detail.html',{'data':course})
+
+def course_duration(request):
+    dur={'python':'4 month','Django':'3 month','postgresql':'1 month','Git':'15 days'}
+    return render(request,'course/'course_detail.html',{'data':'dur'})
