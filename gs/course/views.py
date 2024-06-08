1ba67b482cb3996a38ad4bd744dc219542be15bc
golu <golupyproject@gmail.com>
2024-06-08 18:44:55 +0530

    course and duration both are write in a one single fun

--- gs/course/views.py
+++ gs/course/views.py
@@ -6,13 +6,6 @@ def index(request):
 
 def course_details(request):
     course=['Python','Django','PostgreSql','Mysql','Git&Github']
-    #course=""
-    return render(request,'course/course_detail.html',{'data':course})
-
-def course_duration(request):
     dur={'python':'4 month','Django':'3 month','postgresql':'1 month','Git':'15 days'}
-<<<<<<< HEAD
-    return render(request,'course/course_detail.html',{'data':'dur'})
-=======
-    return render(request,'course/course_detail.html',{'data_dur':'dur'})
->>>>>>> 84bbc9675d6897ec580f242cd2bd944e12283228
+    #course=""
+    return render(request,'course/course_detail.html',{'data':course,'data_dur':dur})
